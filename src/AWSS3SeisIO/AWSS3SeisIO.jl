311824e840e363eb6e6405dd2e66085d12c6d4ca
Henryk Modzelewski <henryk_modzelewski@mac.com>
2018-07-31 17:52:08 -0700

    added my_s3_get_range range reader

--- src/AWSS3SeisIO/AWSS3SeisIO.jl
+++ src/AWSS3SeisIO/AWSS3SeisIO.jl
@@ -1,12 +1,14 @@
 module AWSS3SeisIO
     using AWSCore
         using AWSS3
+    using Retry
     using SeisIO
 
     # SeisIO imports
     import SeisIO: segy_read
 
     #Reader
+    include("read/AWSS3extras.jl")
     include("read/segy_read.jl")
 
 end # module
